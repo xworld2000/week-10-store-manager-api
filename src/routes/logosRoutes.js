6288006e94ed5c7852d4345307a0292c26e3dde6
xworld2000 <xworld2000@gmail.com>
2020-08-06 19:40:48 -0400

    Adds functionality to the post logos http test

--- src/routes/logosRoutes.js
+++ src/routes/logosRoutes.js
@@ -1,9 +1,18 @@
 
 const router = require('express').Router();
-const {deleteLogo, updateLogo, createLogo, getLogos} = require('../database/logos');
+const { deleteLogo, updateLogo, createLogo, getLogos } = require('../database/logos');
 
 router.get('/', async (req, res) => {
   res.send(await getLogos());
+});
+router.post('/', async (apiRequest, apiResponse) => {
+  const newLogo = apiRequest.body;
+  await createLogo(newLogo);
+  apiResponse.send({
+    message: 'New logo created.',
+    allLogos: await getLogos(),
+    thanks: true
+  });
 });
 
 module.exports = router;
